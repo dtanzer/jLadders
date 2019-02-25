7d5e98d593761a8a5a3e95db17b6649e54b0a6be
David Tanzer <business@davidtanzer.net>
2019-02-25 08:05:15 +0100

    Initial import with one failing (dummy) test

---
+++ src/main/java/net/davidtanzer/jladders/Ladders.java
@@ -0,0 +1,4 @@
+package net.davidtanzer.jladders;
+
+public class Ladders {
+}
---
+++ src/test/java/net/davidtanzer/jladders/LaddersTest.java
@@ -0,0 +1,14 @@
+package net.davidtanzer.jladders;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.fail;
+
+public class LaddersTest {
+	private Ladders ladders = new Ladders();
+
+	@Test
+	void changeMe() {
+		fail("Change Me!");
+	}
+}
