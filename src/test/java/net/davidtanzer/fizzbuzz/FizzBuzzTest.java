5126eb5efef860e6e121e7b577e6f362811b10ad
David Tanzer <business@davidtanzer.net>
2019-02-25 09:43:26 +0100

    Working (WIP works).

--- src/test/java/net/davidtanzer/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/net/davidtanzer/fizzbuzz/FizzBuzzTest.java
@@ -8,13 +8,20 @@ import java.util.List;
 import static org.assertj.core.api.Java6Assertions.assertThat;
 
 public class FizzBuzzTest {
+
 	@Test
 	public void OneReturnsOne() {
 		List<String> result = fizzBuzz();
 		assertThat(result.get(0)).isEqualTo("1");
 	}
 
+	@Test
+	public void TwoReturnsTwo() {
+		List<String> result = fizzBuzz();
+		assertThat(result.get(1)).isEqualTo("2");
+	}
+
 	private List<String> fizzBuzz() {
-		return Arrays.asList("1");
+		return Arrays.asList("1", "2");
 	}
 }
