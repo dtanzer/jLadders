b6ad2976701d80e16e644a41ed1faee2cd67f58a
David Tanzer <business@davidtanzer.net>
2019-02-25 10:01:46 +0100

    Working.

--- src/test/java/net/davidtanzer/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/net/davidtanzer/fizzbuzz/FizzBuzzTest.java
@@ -2,7 +2,6 @@ package net.davidtanzer.fizzbuzz;
 
 import org.junit.jupiter.api.Test;
 
-import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
@@ -10,34 +9,34 @@ import java.util.stream.IntStream;
 import static org.assertj.core.api.Java6Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
-public class FizzBuzzTest {
+class FizzBuzzTest {
 
 	@Test
-	public void OneReturnsOne() {
+	void OneReturnsOne() {
 		List<String> result = fizzBuzz();
 		assertThat(result.get(1-1)).isEqualTo("1");
 	}
 
 	@Test
-	public void TwoReturnsTwo() {
+	void TwoReturnsTwo() {
 		List<String> result = fizzBuzz();
 		assertThat(result.get(2-1)).isEqualTo("2");
 	}
 
 	@Test
-	public void Returns99Lines() {
+	void Returns99Lines() {
 		List<String> result = fizzBuzz();
 		assertThat(result.size()).isEqualTo(99);
 	}
 
 	@Test
-	public void ThreeReturnsFizz() {
+	void ThreeReturnsFizz() {
 		List<String> result = fizzBuzz();
 		assertThat(result.get(3-1)).isEqualTo("Fizz");
 	}
 
 	@Test
-	public void MultipleOfThreeReturnsFizz() {
+	void MultipleOfThreeReturnsFizz() {
 		List<String> result = fizzBuzz();
 		assertAll(IntStream.iterate(3, i -> i+3)
 				.limit(33)
@@ -46,13 +45,13 @@ public class FizzBuzzTest {
 	}
 
 	@Test
-	public void FiveReturnsBuzz() {
+	void FiveReturnsBuzz() {
 		List<String> result = fizzBuzz();
 		assertThat(result.get(5-1)).isEqualTo("Buzz");
 	}
 
 	@Test
-	public void MultipleOfFiveReturnsBuzz() {
+	void MultipleOfFiveReturnsBuzz() {
 		List<String> result = fizzBuzz();
 		assertAll(IntStream.iterate(5, i -> i+5).
 				limit(19)
